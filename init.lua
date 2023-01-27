ef91a7b9160bd2cb1f52899966c1cb2368e82d52
Leandro Solidum <lsolidum@freeus.com>
2023-01-27 15:44:57 -0800

    dont maximize windows

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 function FocusAndMaximizeApp(appName)
     hs.application.launchOrFocus(appName)
-    local app = hs.appfinder.appFromName(appName)
-    for _, window in pairs(app:allWindows()) do
-        window:maximize()
-    end
+    -- local app = hs.appfinder.appFromName(appName)
+    -- for _, window in pairs(app:allWindows()) do
+    --     window:maximize()
+    -- end
 end
 
 function CycleWindows()
@@ -12,59 +12,32 @@ function CycleWindows()
     local all = app:allWindows()
     if app == nil or win == nil or all == nil then return end
     -- grabs the last window, which become the first window when focused
-    local nextWindow = all[#all] 
+    local nextWindow = all[#all]
     nextWindow:focus()
+end
+
+function CreateMehShortcut(appName, letters)
+    for _, letter in ipairs(letters) do
+        hs.hotkey.bind({ "shift", "alt", "ctrl" }, letter, 
+            function() 
+                FocusAndMaximizeApp(appName) 
+            end
+        )
+    end
 end
 
 hs.hotkey.bind({ "shift", "alt", "ctrl" }, "ESCAPE", function()
     CycleWindows()
 end)
 
-hs.hotkey.bind({ "shift", "alt", "ctrl" }, "F", function()
-    FocusAndMaximizeApp("Fork")
-end)
-
-hs.hotkey.bind({ "shift", "alt", "ctrl" }, "P", function()
-    FocusAndMaximizeApp("Postman")
-end)
-
-hs.hotkey.bind({ "shift", "alt", "ctrl" }, "B", function()
-    FocusAndMaximizeApp("Logos")
-end)
-
-hs.hotkey.bind({ "shift", "alt", "ctrl" }, "A", function()
-    FocusAndMaximizeApp("Alacritty")
-end)
-
-hs.hotkey.bind({ "shift", "alt", "ctrl" }, "R", function()
-    FocusAndMaximizeApp("Nirvana")
-end)
-
-hs.hotkey.bind({ "shift", "alt", "ctrl" }, "S", function()
-    FocusAndMaximizeApp("Spotify")
-end)
-
-hs.hotkey.bind({ "shift", "alt", "ctrl" }, "T", function()
-    FocusAndMaximizeApp("Microsoft Teams")
-end)
-
-hs.hotkey.bind({ "shift", "alt", "ctrl" }, "G", function()
-    FocusAndMaximizeApp("Google Chrome")
-end)
-
-hs.hotkey.bind({ "shift", "alt", "ctrl" }, "X", function()
-    FocusAndMaximizeApp("Xcode")
-end)
-
-hs.hotkey.bind({ "shift", "alt", "ctrl" }, "C", function()
-    FocusAndMaximizeApp("Visual Studio Code")
-end)
-
--- mnemonic: database
-hs.hotkey.bind({ "shift", "alt", "ctrl" }, "D", function()
-    FocusAndMaximizeApp("Studio 3T")
-end)
-
-hs.hotkey.bind({ "shift", "alt", "ctrl" }, "M", function()
-    FocusAndMaximizeApp("Messages")
-end)
+CreateMehShortcut("Alacritty", { "A" })
+CreateMehShortcut("Fork", { "F" })
+CreateMehShortcut("Google Chrome", { "G" })
+CreateMehShortcut("Inkdrop", { "I", "R" })
+CreateMehShortcut("Messages", { "M" })
+CreateMehShortcut("Microsoft Teams", { "S", "W"})
+CreateMehShortcut("Postman", { "P" })
+CreateMehShortcut("Studio 3T", { "D" }) -- D for database
+CreateMehShortcut("Todoist", { "T" })
+CreateMehShortcut("Visual Studio Code", { "C", "V" })
+-- CreateMehShortcut("Xcode", { "X" })
